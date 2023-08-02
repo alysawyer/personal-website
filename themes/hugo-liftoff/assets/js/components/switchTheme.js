a615598ed5f00e64bea9006eca9a2523b159e0d5
Alyssa Sawyer <alyssa@vbm.com>
2023-08-01 18:51:51 -0700

    eliminate dark theme switch

--- themes/hugo-liftoff/assets/js/components/switchTheme.js
+++ themes/hugo-liftoff/assets/js/components/switchTheme.js
@@ -9,31 +9,13 @@ function switchTheme() {
       resetTheme();
     });
 
-    function initTheme() {
-      let lsItem = localStorage.getItem('themeSwitch');
-      let darkThemeSelected = false;
-      if (lsItem !== null) {
-        darkThemeSelected = lsItem === 'dark';
-      } else {
-        darkThemeSelected = window.matchMedia('(prefers-color-scheme: dark)').matches;
-      }
-
-      themeSwitch.checked = darkThemeSelected;
-      resetTheme();
-    }
 
     function resetTheme() {
       if (themeSwitch.checked) {
-        document.body.setAttribute('data-theme', 'dark');
-        localStorage.setItem('themeSwitch', 'dark');
+        
+        console.log('hi')
       } else {
-        document.body.removeAttribute('data-theme');
-        localStorage.setItem('themeSwitch', 'light');
-      }
-
-      // Reset Disqus to match new color scheme
-      if (typeof DISQUS !== "undefined") {
-          DISQUS.reset({ reload: true });
+        console.log('bye')
       }
     }
   }
